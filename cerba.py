4bd209118321c8da4a1e4c8bafcd4c449788469b
halfiHamza <39138552+halfiHamza@users.noreply.github.com>
2021-02-23 14:58:00 +0100

    command line

--- cerba.py
+++ cerba.py
@@ -1,22 +1,26 @@
 _Author_ = 'Halfi Hamza'
 _Gol_ = 'Scraping Cerba pdf'
 
-import fitz, io, json
+import fitz, io, json, sys, argparse
+from sys import stdout
+from os import remove
+from os.path import isfile
 
 
-def select_pages(start_page, end_page):
+def select_pages(first, last):
     # read pdf fitz like objects
     doc = fitz.open("Catalogue_des_examens_spécialisés_.pdf")
-
+    
     # load pages
-    for page_number in range(start_page, end_page):
+    for page_number in range(first, last):
+        stdout.write("\rLoad Page N°: %i page" % page_number)
+        stdout.flush()
         page = doc.loadPage(page_number)
         # convert page to text
         page_text = page.getText("text")
         # write output to text file
-        with io.open("output.txt", 'a+', encoding="utf-8") as text:
+        with io.open("output.txt", mode='a+', encoding="utf-8") as text:
             text.write(page_text)
-
 
 def generate_data(array):
     # variables
@@ -62,8 +66,9 @@ def generate_data(array):
             data['délai'] = result
             
             # dump data to json file
-            with open('cerba.json', 'a', encoding='utf-8') as j:
+            with open('cerba_ouput.json', mode='a+', encoding='utf-8') as j:
                 json.dump(data, j, ensure_ascii=False, indent=4)
+                
 
 def parsing():
     # Variables
@@ -73,8 +78,9 @@ def parsing():
     # read output and generate data
     with io.open("output.txt", 'r', encoding="utf-8") as f:
         lines = f.readlines()
-    
         for i, line in enumerate(lines):
+            stdout.write("\rGenerate Lines: %i line" % i)
+            stdout.flush()
             value = line.replace('\ufeff', '').replace("\n", "")
             if value.startswith("AC") and len(value) > 3:
                 start_point = i
@@ -82,4 +88,21 @@ def parsing():
                 end_point = i + 1
                 generate_data(lines[start_point:end_point])
 
-parsing()
+
+if __name__ == "__main__":
+    # command line argument input
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-F', '--first_page', default=40, help='number of first page', type=int)
+    parser.add_argument('-L', '--last_page', default=50, help='number of last page', type=int)
+    args = parser.parse_args()
+
+    # remove files befor
+    if isfile('output.txt'):
+        remove('output.txt')
+    if isfile('cerba_ouput.json'):
+        remove('cerba_ouput.json')
+
+    print('\nStart Reading pages\n')
+    select_pages(first=args.first_page, last=args.last_page)
+    print('\n')
+    parsing()
